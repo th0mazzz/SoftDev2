23540e6848eaa2f63b9de88d8faca0a46a2a8669
th0mazzz <thomaszhao2001@gmail.com>
2019-01-31 16:06:21 -0500

    Added comments explaining beginPath and offset

--- 01_canvas/clickdraw.js
+++ 01_canvas/clickdraw.js
@@ -4,10 +4,10 @@ var context = slate.getContext("2d");
 var clearButton = document.getElementById("clearButton");
 var toggleButton = document.getElementById("toggleButton");
 var modeDisplay = document.getElementById("modeDisplay");
-var mode = "Rektangle"
+var state = "Rektangle"
 
-//Starts off the mode display with current mode
-modeDisplay.innerHTML = mode;
+//Starts off the mode display with current state
+modeDisplay.innerHTML = state;
 
 //Clears the entire canvas
 var clearCanvas = function(e) {
@@ -21,28 +21,46 @@ var clearCanvas = function(e) {
 var draw = function(e) {
     //console.log(e);
     //console.log(context);
-    if(mode == "Rektangle"){
+    if(state == "Rektangle"){
 	context.fillRect(e.offsetX, e.offsetY, 100, 50);
     }
     else{
 	context.beginPath();
+	/*
+	  CanvasRenderingContext2D.beginPath();
+
+	  This statement clears path information that may previously already be there.
+	  In the context of this code, it ensures that the circles-dots are individual
+	  circles and are not connected in any way.
+	*/
+	
 	context.ellipse(e.offsetX, e.offsetY, 5, 5, 0, 0, 2 * Math.PI);
+	/*
+	 event.offsetX
+	 event.offsetY
+
+	 The offset properties contain the x-coordinate and y-coordinate properties relative
+	 to the target element. The target element is part of the MouseEvent properties.
+	 In the context of this code, they provide the x-coordinates and the y-coordinates
+	 of the MouseEvent relative to the HTML canvas element.
+	*/
+	
 	context.stroke();
     }
     console.log(e)
     console.log(e.offsetX);
 }
 
-//Toggles mode and updates mode display
+//Toggles state and updates mode display
 var toggle = function(e) {
-    if(mode == "Rektangle"){
-	mode = "Sirkle Dot Thing";
+    if(state == "Rektangle"){
+	state = "Sirkle Dot Thing";
     }
     else{
-	mode = "Rektangle"
+	state = "Rektangle"
     }
-    modeDisplay.innerHTML = mode;
-    console.log(mode)
+    modeDisplay.innerHTML = state;
+    console.log(state)
 }
 
 //Event Listeners
